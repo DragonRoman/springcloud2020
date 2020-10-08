5b23c8b8b351c72551d272780c5b92dcc1685c37
Roman <John6984@163.com>
2020-10-08 20:22:13 +0800

    SpringCloud MicroServices Project

---
+++ cloud-configuration-client3355/src/main/java/com/minger/springcloud/ConfigurationClientMain3355.java
@@ -0,0 +1,13 @@
+package com.minger.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigurationClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigurationClientMain3355.class,args);
+    }
+}
